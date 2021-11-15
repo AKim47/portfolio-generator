a0fd20973544be43e10d30fd1849d0fd1ed17ca5
AKim47 <abel.kim.22@gmail.com>
2021-11-14 22:56:28 -0600

    installed Inquirer

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', pageeHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 /*
 console.log(profileDataArgs);
 
